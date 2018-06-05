82f4434b489e50165df6cfc24a63dc85fc08b297
Alex Calispa <36059357+AlexCD7@users.noreply.github.com>
2018-06-04 21:35:41 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+namespace alex.Aritmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double valor1 ;
+            Console.WriteLine("----CALCULADORA----  ");
+            Console.WriteLine("Ingrese un valor : ");
+            valor1 = double.Parse(Console.ReadLine());
+            var calNet = new Calculadora();
+            var resultado =calNet.Sumar (valor1,3);
+            resultado=calNet.Restar(resultado,3);
+            resultado=calNet.Raiz(resultado);
+            resultado=calNet.Dividir(resultado,2);
+            resultado=calNet.Multiplicar(resultado,4);
+            Console.WriteLine("El resultado de la operacion es : "+ resultado);
+            Console.WriteLine("\n\n\tPresione < ENTER > para salir . . .");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ calculadora.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+class Calculadora{
+    private double memoria=0;
+     
+    public double Sumar(double sumando1,double sumando2)
+            {
+               
+                 memoria = sumando1+sumando2;
+                return memoria;
+            }
+ 
+
+    public double Sumar(double sumando)
+            {
+                        
+                        
+                memoria= memoria + sumando;
+                return memoria;
+                        
+            }
+
+    public double Restar(double minuendo,double sustraendo)
+            {
+                        
+                        
+                 memoria= minuendo-sustraendo;
+                return memoria;
+            }
+
+    public double Restar(double resto)
+            {
+                        
+                        
+                memoria= memoria - resto;
+                return memoria;
+                        
+            }
+
+
+     public double Multiplicar(double multiplicando,double multplicador)
+    {
+                
+                
+                memoria= multiplicando*multplicador;
+                return memoria;
+    }
+
+    public double Multiplicar(double multiplicando1)
+            {
+                        
+                        
+               memoria = memoria*multiplicando1;
+               return memoria;
+            }
+
+    public double Dividir(double dividendo ,double divisor )
+    {
+               
+               
+             memoria= dividendo/divisor;
+             return memoria;
+    } 
+
+    public double Dividir(double dividendo1)
+            {
+                        
+                        
+                memoria= memoria / dividendo1;
+                return memoria;
+                        
+            }
+
+
+
+
+    public double Raiz(double radicando)
+        {
+                    
+                    
+                memoria= Math.Sqrt(radicando);
+                return memoria;
+                    
+        }
+
+        
+
+    public double Raiz()
+        {
+                    
+                    memoria= Math.Sqrt(memoria);
+                    return memoria;
+                    
+        }
+
+}
